1797f1f80efcfb12cd5489eb1556a39b1bb1c681
Alia Robinson <robinsat@rose-hulman.edu>
2017-04-30 20:29:20 -0400

    Documents read to file

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,9 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -6,7 +12,12 @@ import java.util.Scanner;
  */
 public class Main {
 
+    static Map<String, List<String>> hitList;
+
     public static void main(String[] args) {
+
+        curateDocuments();
+
         while(true) {
             System.out.println("Enter search query, or 'quit' to exit: ");
             Scanner scanner = new Scanner(System.in);
@@ -19,5 +30,22 @@ public class Main {
             // Find relevant documents
         }
 
+    }
+
+    static void curateDocuments() {
+        hitList = new HashMap<String, List<String>>();
+        File corpusDirectory = new File("corpus");
+        File[] list = corpusDirectory.listFiles();
+        for (File file : list) {
+            processDocument(file);
+        }
+    }
+
+    static void processDocument(File document) {
+        try {
+            String content = new String(Files.readAllBytes(document.toPath()));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
