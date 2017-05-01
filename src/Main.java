f39f9cfdfbe9b588a2e22fcd40612846c0ba80c8
Alia Robinson <robinsat@rose-hulman.edu>
2017-04-30 20:00:26 -0400

    Implemented command-line querying

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 /**
+ * Runnable main class
  * Created by robinsat on 4/30/2017.
  */
 public class Main {
 
     public static void main(String[] args) {
-        
+        while(true) {
+            System.out.println("Enter search query, or 'quit' to exit: ");
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.next();
+
+            if(input.equalsIgnoreCase("quit")) {
+                System.exit(0);
+            }
+
+            // Find relevant documents
+        }
+
     }
 }
